aaf86944752e38e4f046ff9760b2818eaa4149a8
Santa <santazaaa@gmail.com>
2017-11-24 18:28:24 +0700

    Run game instance

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,12 +1,20 @@
 package controllers
 
 import (
+	"github.com/satori/go.uuid"
 	"github.com/revel/revel"
+	"os/exec"
 	"santa/game-server-mgr/app"
 )
 
 type App struct {
 	*revel.Controller
+}
+
+type StartGameResponse struct {
+	GameID         string	`json:"GameID"`
+	ServerHostname string	`json:"ServerHostname"`
+	ServerPort     int		`json:"ServerPort"`
 }
 
 func (c App) Index() revel.Result {
@@ -17,4 +25,31 @@ func (c App) TestIncr() revel.Result {
 	app.RedisCli.Incr("count")
 	r, _ := app.RedisCli.Get("count").Result()
 	return c.RenderText("count: " + r)
+}
+
+func (c App) StartGame() revel.Result {
+	gameID := uuid.NewV4().String()
+
+	revel.INFO.Println("[app::StartGame] Starting new game instance id = " + gameID)
+
+	go func() {
+		cmd := exec.Command("./test-server", "-batchmode", "-nographics", "-logfile", "log.txt")
+		cmd.Dir = "builds/test-server.app/Contents/MacOS/"
+		_, err := cmd.Output()
+		if err != nil {
+			println(err.Error())
+		}
+		revel.INFO.Println("[app::StartGame] Closed game instance id = " + gameID)
+	}()
+	
+	data := make(map[string]interface{})
+	data["error"] = nil
+	res := StartGameResponse{
+		GameID:         gameID,
+		ServerHostname: "localhost",
+		ServerPort:     7777,
+	}
+	data["data"] = res
+	
+	return c.RenderJSON(data)
 }
